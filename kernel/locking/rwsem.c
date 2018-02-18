f577d3c377e98870d4946e11f98cb0a1fbc0bd5e
Arjan van de Ven <arjan@linux.intel.com>
2018-02-18 23:35:41 +0000

    locking: rwsem: spin faster
    tweak rwsem owner spinning a bit
    
    Signed-off-by: dopaemon <polarisdp@gmail.com>

--- kernel/locking/rwsem.c
+++ kernel/locking/rwsem.c
@@ -724,6 +724,7 @@ rwsem_spin_on_owner(struct rw_semaphore *sem, unsigned long nonspinnable)
 	enum owner_state state;
 	int cnt = 0;
 	bool time_out = false;
+	int i = 0;
 
 	owner = rwsem_owner_flags(sem, &flags);
 	state = rwsem_owner_state(owner, flags, nonspinnable);
@@ -760,7 +761,8 @@ rwsem_spin_on_owner(struct rw_semaphore *sem, unsigned long nonspinnable)
 			break;
 		}
 
-		cpu_relax();
+		if (i++ > 1000)
+			cpu_relax();
 	}
 	rcu_read_unlock();
 
