13cb7ef90bc325d54f6ff062781ba12eac5d6e86
Park Ju Hyung <qkrwngud825@gmail.com>
2019-09-30 22:13:16 +0900

    lz4: remove unused functions
    Signed-off-by: Park Ju Hyung <qkrwngud825@gmail.com>
    Signed-off-by: dopaemon <polarisdp@gmail.com>

--- include/linux/lz4.h
+++ include/linux/lz4.h
@@ -134,6 +134,7 @@ typedef union {
 	LZ4HC_CCtx_internal internal_donotuse;
 } LZ4_streamHC_t;
 
+#if 0
 /*
  * LZ4_streamDecode_t - information structure to track an
  *	LZ4 stream during decompression.
@@ -150,6 +151,7 @@ typedef union {
 	unsigned long long table[LZ4_STREAMDECODESIZE_U64];
 	LZ4_streamDecode_t_internal internal_donotuse;
 } LZ4_streamDecode_t;
+#endif
 
 /*-************************************************************************
  *	SIZE OF STATE
@@ -198,6 +200,7 @@ static inline int LZ4_compressBound(size_t isize)
 int LZ4_compress_default(const char *source, char *dest, int inputSize,
 	int maxOutputSize, void *wrkmem);
 
+#if 0
 /**
  * LZ4_compress_fast() - As LZ4_compress_default providing an acceleration param
  * @source: source address of the original data
@@ -245,6 +248,7 @@ static int LZ4_compress_fast(const char *source, char *dest, int inputSize,
  */
 static int LZ4_compress_destSize(const char *source, char *dest, int *sourceSizePtr,
 	int targetDestSize, void *wrkmem);
+#endif
 
 /*-************************************************************************
  *	Decompression Functions
@@ -347,6 +351,7 @@ int LZ4_decompress_safe_partial(const char *source, char *dest,
 int LZ4_compress_HC(const char *src, char *dst, int srcSize, int dstCapacity,
 	int compressionLevel, void *wrkmem);
 
+#if 0
 /**
  * LZ4_resetStreamHC() - Init an allocated 'LZ4_streamHC_t' structure
  * @streamHCPtr: pointer to the 'LZ4_streamHC_t' structure
@@ -645,5 +650,6 @@ static int LZ4_decompress_safe_usingDict(const char *source, char *dest,
  */
 static int LZ4_decompress_fast_usingDict(const char *source, char *dest,
 	int originalSize, const char *dictStart, int dictSize);
+#endif
 
 #endif
