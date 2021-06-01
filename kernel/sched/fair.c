95da65c9ae9422b9f3c010911db9a59a5ed40145
Dietmar Eggemann <dietmar.eggemann@arm.com>
2021-06-01 10:36:16 +0200

    sched/fair: Return early from update_tg_cfs_load() if delta == 0
    In case the _avg delta is 0 there is no need to update se's _avg
    (level n) nor cfs_rq's _avg (level n-1). These values stay the same.
    
    Since cfs_rq's _avg isn't changed, i.e. no load is propagated down,
    cfs_rq's _sum should stay the same as well.
    
    So bail out after se's _sum has been updated.
    
    Change-Id: I134d3d1c7baffe9af6ee8eed03308bdbee8e4506
    Signed-off-by: Dietmar Eggemann <dietmar.eggemann@arm.com>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Reviewed-by: Vincent Guittot <vincent.guittot@linaro.org>
    Link: https://lore.kernel.org/r/20210601083616.804229-1-dietmar.eggemann@arm.com

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -3692,9 +3692,12 @@ update_tg_cfs_load(struct cfs_rq *cfs_rq, struct sched_entity *se, struct cfs_rq
 	if (!delta_avg)
 		return;
 
-	delta = load_avg - se->avg.load_avg;
-
 	se->avg.load_sum = runnable_sum;
+
+	delta = load_avg - se->avg.load_avg;
+	if (!delta)
+		return;
+
 	se->avg.load_avg = load_avg;
 
 	add_positive(&cfs_rq->avg.load_avg, delta);
