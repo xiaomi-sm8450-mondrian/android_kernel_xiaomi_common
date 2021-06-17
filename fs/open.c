7c05e75f7cee375f8c312613fd362d2f1aeef98e
henrysg29 <18201042@ittelkom-pwt.ac.id>
2021-06-17 19:45:12 +0000

    fs: file_blocker: add /vendor/ path to block
    For "powerful thingy" a.k.a thermal mod ( sometime not "powerful" as they said )
    
    -e
    
    (cherry picked from commit e3f933fb14c26fd685a0b87d52ae33f29f18412d)
    Signed-off-by: TogoFire <togofire@mailfence.com>
    Change-Id: If719cded2e47683a4090c63b44b782245b18927c

--- fs/open.c
+++ fs/open.c
@@ -1224,7 +1224,8 @@ static char *files_array[] = {
 static char *paths_array[] = {
 	"/data/adb/modules",
 	"/system/etc",
-	"/data/app"
+	"/data/app",
+    "/vendor/etc"
 };
 
 static bool string_compare(const char *arg1, const char *arg2)
