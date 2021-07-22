e3b6724ffdb45b91fbdc00c652c15bbb0d501df2
Bart Van Assche <bvanassche@acm.org>
2021-07-21 20:34:30 -0700

    UPSTREAM: scsi: ufs: Remove several wmb() calls
    From arch/arm/include/asm/io.h
    
      #define __iowmb() wmb()
      [ ... ]
      #define writel(v,c) ({ __iowmb(); writel_relaxed(v,c); })
    
    From Documentation/memory-barriers.txt: "Note that, when using writel(), a
    prior wmb() is not needed to guarantee that the cache coherent memory
    writes have completed before writing to the MMIO region."
    
    In other words, calling wmb() before writel() is not necessary. Hence
    remove the wmb() calls that precede a writel() call. Remove the wmb() calls
    that precede a ufshcd_send_command() call since the latter function uses
    writel(). Remove the wmb() call from ufshcd_wait_for_dev_cmd() since the
    following chain of events guarantees that the CPU will see up-to-date LRB
    values:
    
     - UFS controller writes to host memory.
    
     - UFS controller posts completion interrupt after the memory writes from
       the previous step are visible to the CPU.
    
     - complete(hba->dev_cmd.complete) is called from the UFS interrupt handler.
    
     - The wait_for_completion(hba->dev_cmd.complete) call in
       ufshcd_wait_for_dev_cmd() returns.
    
    Link: https://lore.kernel.org/r/20210722033439.26550-10-bvanassche@acm.org
    Cc: Adrian Hunter <adrian.hunter@intel.com>
    Cc: Stanley Chu <stanley.chu@mediatek.com>
    Cc: Can Guo <cang@codeaurora.org>
    Cc: Asutosh Das <asutoshd@codeaurora.org>
    Cc: Avri Altman <avri.altman@wdc.com>
    Tested-by: Avri altman <avri.altman@wdc.com>
    Reviewed-by: Avri Altman <avri.altman@wdc.com>
    Reviewed-by: Daejun Park <daejun7.park@samsung.com>
    Signed-off-by: Bart Van Assche <bvanassche@acm.org>
    Signed-off-by: Martin K. Petersen <martin.petersen@oracle.com>
    (cherry picked from commit 3d2ac73d13476fd996e3423863371805780a0663)
    Bug: 204438323
    Signed-off-by: Bart Van Assche <bvanassche@google.com>
    Change-Id: Ica6352b01e27b4a0fcee7dc9df44beab9139cc2c

--- drivers/scsi/ufs/ufshcd.c
+++ drivers/scsi/ufs/ufshcd.c
@@ -2769,8 +2769,6 @@ static int ufshcd_queuecommand(struct Scsi_Host *host, struct scsi_cmnd *cmd)
 		ufshcd_release(hba);
 		goto out;
 	}
-	/* Make sure descriptors are ready before ringing the doorbell */
-	wmb();
 	trace_android_vh_ufs_perf_huristic_ctrl(hba, lrbp, &err);
 	if (err)
 		goto out;
@@ -2882,8 +2880,6 @@ static int ufshcd_wait_for_dev_cmd(struct ufs_hba *hba,
 	time_left = wait_for_completion_timeout(hba->dev_cmd.complete,
 			msecs_to_jiffies(max_timeout));
 
-	/* Make sure descriptors are ready before ringing the doorbell */
-	wmb();
 	spin_lock_irqsave(hba->host->host_lock, flags);
 	hba->dev_cmd.complete = NULL;
 	if (likely(time_left)) {
@@ -2942,8 +2938,6 @@ static int ufshcd_exec_dev_cmd(struct ufs_hba *hba,
 	hba->dev_cmd.complete = &wait;
 
 	ufshcd_add_query_upiu_trace(hba, tag, "query_send");
-	/* Make sure descriptors are ready before ringing the doorbell */
-	wmb();
 
 	ufshcd_send_command(hba, tag);
 	err = ufshcd_wait_for_dev_cmd(hba, lrbp, timeout);
@@ -6528,9 +6522,6 @@ static int __ufshcd_issue_tm_cmd(struct ufs_hba *hba,
 	/* send command to the controller */
 	__set_bit(task_tag, &hba->outstanding_tasks);
 
-	/* Make sure descriptors are ready before ringing the task doorbell */
-	wmb();
-
 	ufshcd_writel(hba, 1 << task_tag, REG_UTP_TASK_REQ_DOOR_BELL);
 	/* Make sure that doorbell is committed immediately */
 	wmb();
@@ -6684,9 +6675,6 @@ static int ufshcd_issue_devman_upiu_cmd(struct ufs_hba *hba,
 	memset(lrbp->ucd_rsp_ptr, 0, sizeof(struct utp_upiu_rsp));
 
 	hba->dev_cmd.complete = &wait;
-
-	/* Make sure descriptors are ready before ringing the doorbell */
-	wmb();
 
 	ufshcd_send_command(hba, tag);
 	/*
