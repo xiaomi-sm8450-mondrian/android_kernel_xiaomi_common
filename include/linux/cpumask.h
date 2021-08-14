3653f4fca5f0b685a65e2a7b5be6e85b529f13ff
Yury Norov <yury.norov@gmail.com>
2021-08-14 14:17:05 -0700

    cpumask: replace cpumask_next_* with cpumask_first_* where appropriate
    cpumask_first() is a more effective analogue of 'next' version if n == -1
    (which means start == 0). This patch replaces 'next' with 'first' where
    things look trivial.
    
    There's no cpumask_first_zero() function, so create it.
    
    Signed-off-by: Yury Norov <yury.norov@gmail.com>
    Tested-by: Wolfram Sang <wsa+renesas@sang-engineering.com>

--- include/linux/cpumask.h
+++ include/linux/cpumask.h
@@ -158,6 +158,11 @@ static inline unsigned int cpumask_first(const struct cpumask *srcp)
 	return 0;
 }
 
+static inline unsigned int cpumask_first_zero(const struct cpumask *srcp)
+{
+	return 0;
+}
+
 static inline unsigned int cpumask_first_and(const struct cpumask *srcp1,
 					     const struct cpumask *srcp2)
 {
@@ -229,6 +234,17 @@ static inline int cpumask_any_and_distribute(const struct cpumask *src1p,
 static inline unsigned int cpumask_first(const struct cpumask *srcp)
 {
 	return find_first_bit(cpumask_bits(srcp), nr_cpumask_bits);
+}
+
+/**
+ * cpumask_first_zero - get the first unset cpu in a cpumask
+ * @srcp: the cpumask pointer
+ *
+ * Returns >= nr_cpu_ids if all cpus are set.
+ */
+static inline unsigned int cpumask_first_zero(const struct cpumask *srcp)
+{
+	return find_first_zero_bit(cpumask_bits(srcp), nr_cpumask_bits);
 }
 
 /**
