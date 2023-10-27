ce309fa5614f18463fd1b602ca36e397bd7f62ea
GhostAlee <aleegosth15@hotmail.com>
2023-10-27 10:16:08 -0600

    Cpufreq_walt: Remove tracing * Taken to: https://github.com/diphons/D8G_Kernel_Marble/commit/1e745fbfcca9bcc5aeaec0b6db76f91cc4a9c8f2

--- kernel/sched/walt/cpufreq_walt.c
+++ kernel/sched/walt/cpufreq_walt.c
@@ -234,9 +234,6 @@ static unsigned int get_next_freq(struct waltgov_policy *wg_policy,
 			freq = wg_policy->tunables->adaptive_high_freq;
 	}
 
-	trace_waltgov_next_freq(policy->cpu, util, max, raw_freq, freq, policy->min, policy->max,
-				wg_policy->cached_raw_freq, wg_policy->need_freq_update);
-
 	if (wg_policy->cached_raw_freq && freq == wg_policy->cached_raw_freq &&
 		!wg_policy->need_freq_update)
 		return 0;
@@ -381,11 +378,6 @@ static void waltgov_update_freq(struct waltgov_callback *cb, u64 time,
 
 	waltgov_calc_avg_cap(wg_policy, wg_cpu->walt_load.ws,
 			   wg_policy->policy->cur);
-
-	trace_waltgov_util_update(wg_cpu->cpu, wg_cpu->util, wg_policy->avg_cap,
-				wg_cpu->max, wg_cpu->walt_load.nl,
-				wg_cpu->walt_load.pl,
-				wg_cpu->walt_load.rtgb_active, flags);
 
 	if (waltgov_should_update_freq(wg_policy, time) &&
 	    !(flags & WALT_CPUFREQ_CONTINUE)) {
