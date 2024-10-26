3bb8914df92ff63b9220ed39b1b25f58ebc3597d
Nahuel Gómez <nahuelgomez329@gmail.com>
2024-10-26 15:31:18 -0300

    kernel: sysctl: add init protection to common mm-related nodes
    The protected nodes are:
    * dirty_ratio
    * dirty_background_ratio
    * dirty_bytes
    * dirty_background_bytes
    * dirty_expire_centisecs
    * dirty_writeback_centisecs
    * swappiness
    
    This approach is inspired by [1] and makes use of the node tampering blacklist.
    
    [1]: https://github.com/GuidixX/kernel_xiaomi_lavender/commit/239efdc2635d7c6c150616edcfc41f4bd69d3ad6
    
    Signed-off-by: Nahuel Gómez <nahuelgomez329@gmail.com>

--- mm/page-writeback.c
+++ mm/page-writeback.c
@@ -38,6 +38,7 @@
 #include <linux/sched/rt.h>
 #include <linux/sched/signal.h>
 #include <linux/mm_inline.h>
+#include <linux/binfmts.h>
 #include <trace/events/writeback.h>
 
 #include "internal.h"
@@ -528,6 +529,9 @@ int dirty_background_ratio_handler(struct ctl_table *table, int write,
 {
 	int ret;
 
+	if (task_is_booster(current))
+		return 0;
+
 	ret = proc_dointvec_minmax(table, write, buffer, lenp, ppos);
 	if (ret == 0 && write)
 		dirty_background_bytes = 0;
@@ -539,6 +543,9 @@ int dirty_background_bytes_handler(struct ctl_table *table, int write,
 {
 	int ret;
 	unsigned long old_bytes = dirty_background_bytes;
+
+	if (task_is_booster(current))
+		return 0;
 
 	ret = proc_doulongvec_minmax(table, write, buffer, lenp, ppos);
 	if (ret == 0 && write) {
@@ -558,6 +565,9 @@ int dirty_ratio_handler(struct ctl_table *table, int write, void *buffer,
 	int old_ratio = vm_dirty_ratio;
 	int ret;
 
+	if (task_is_booster(current))
+		return 0;
+
 	ret = proc_dointvec_minmax(table, write, buffer, lenp, ppos);
 	if (ret == 0 && write && vm_dirty_ratio != old_ratio) {
 		writeback_set_ratelimit();
@@ -571,6 +581,9 @@ int dirty_bytes_handler(struct ctl_table *table, int write,
 {
 	unsigned long old_bytes = vm_dirty_bytes;
 	int ret;
+
+	if (task_is_booster(current))
+		return 0;
 
 	ret = proc_doulongvec_minmax(table, write, buffer, lenp, ppos);
 	if (ret == 0 && write && vm_dirty_bytes != old_bytes) {
@@ -2016,6 +2029,9 @@ int dirty_writeback_centisecs_handler(struct ctl_table *table, int write,
 {
 	unsigned int old_interval = dirty_writeback_interval;
 	int ret;
+
+	if (task_is_booster(current))
+		return 0;
 
 	ret = proc_dointvec(table, write, buffer, length, ppos);
 
