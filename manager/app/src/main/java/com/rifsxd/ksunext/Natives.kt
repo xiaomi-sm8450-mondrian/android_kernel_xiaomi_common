3d9feeec96a62b78cc2a6bfb4e646793fc012634
Rifat Azad <rifat.44.azad.rifs@gmail.com>
2024-12-19 21:56:04 +0600

    manager: Dropped down MINIMAL_SUPPORTED_KERNEL to 11071

--- manager/app/src/main/java/com/rifsxd/ksunext/Natives.kt
+++ manager/app/src/main/java/com/rifsxd/ksunext/Natives.kt
@@ -15,14 +15,12 @@ object Natives {
     // 10931: app profile struct add 'version' field
     // 10946: add capabilities
     // 10977: change groups_count and groups to avoid overflow write
-    // 11071: Fix the issue of failing to set a custom SELinux type
-    // 11998: Next experimental modifications.
-    const val MINIMAL_SUPPORTED_KERNEL = 11998
+    // 11071: Fix the issue of failing to set a custom SELinux type.
+    const val MINIMAL_SUPPORTED_KERNEL = 11071
 
     // 11640: Support query working mode, LKM or GKI
-    // 12016: Next now supports GKI -> LKM/KMI modes!
     // when MINIMAL_SUPPORTED_KERNEL > 11640, we can remove this constant.
-    const val MINIMAL_SUPPORTED_KERNEL_LKM = 12016
+    const val MINIMAL_SUPPORTED_KERNEL_LKM = 11648
     const val KERNEL_SU_DOMAIN = "u:r:su:s0"
 
     const val ROOT_UID = 0
