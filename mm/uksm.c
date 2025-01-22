f1e95957b0e507446f1892b3012049d85e66a5b8
dopaemon <polarisdp@gmail.com>
2025-01-22 16:10:55 +0700

    mm/uksm: Remove Const
    Signed-off-by: dopaemon <polarisdp@gmail.com>

--- mm/uksm.c
+++ mm/uksm.c
@@ -109,7 +109,7 @@ int memcmpx86_32(void *s1, void *s2, size_t n)
 /*
  * Check the page is all zero ?
  */
-static int is_full_zero(const void *s1, size_t len)
+static int is_full_zero(void *s1, size_t len)
 {
 	unsigned char same;
 
@@ -151,7 +151,7 @@ int memcmpx86_64(void *s1, void *s2, size_t n)
 	return res;
 }
 
-static int is_full_zero(const void *s1, size_t len)
+static int is_full_zero(void *s1, size_t len)
 {
 	unsigned char same;
 
@@ -169,9 +169,9 @@ static int is_full_zero(const void *s1, size_t len)
 
 #endif
 #else
-static int is_full_zero(const void *s1, size_t len)
+static int is_full_zero(void *s1, size_t len)
 {
-	const unsigned long *src = s1;
+	unsigned long *src = s1;
 	int i;
 
 	len /= sizeof(*src);
