5032462239390269b59e1c68253bc4569d8359c2
Chise Waguri <lqff.yt@gmail.com>
2025-03-03 16:48:17 +0000

    SELinux: Always report Enforcing mode in getenforce and /sys/fs/selinux/enforce
    This commit make user-space tools like `getenforce` and applications
    reading `/sys/fs/selinux/enforce` will always see SELinux as Enforcing.

--- security/selinux/include/security.h
+++ security/selinux/include/security.h
@@ -127,7 +127,7 @@ static inline void selinux_mark_initialized(struct selinux_state *state)
 #ifdef CONFIG_SECURITY_SELINUX_DEVELOP
 static inline bool enforcing_enabled(struct selinux_state *state)
 {
-	return READ_ONCE(state->enforcing);
+    return true;  // Always report Enforcing, even when permissive
 }
 
 static inline void enforcing_set(struct selinux_state *state, bool value)
