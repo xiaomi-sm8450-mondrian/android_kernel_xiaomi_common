86bdf595e049892db9dfc82f3d35e8b96b59e21e
dopaemon <polarisdp@gmail.com>
2025-03-11 23:05:47 +0700

    fs: susfs: Set susfs_sus_su_working_mode to 2
    Enable sus_su, and disable ksu kprobe hooks for su, which means the kernel inline hooks are enabled,
    the same as the su implementaion of non-gki kernel without kprobe supported.
    
    Signed-off-by: dopaemon <polarisdp@gmail.com>

--- fs/susfs.c
+++ fs/susfs.c
@@ -28,8 +28,8 @@ bool susfs_is_log_enabled __read_mostly = true;
 #define SUSFS_LOGI(fmt, ...) if (susfs_is_log_enabled) pr_info("susfs:[%u][%d][%s] " fmt, current_uid().val, current->pid, __func__, ##__VA_ARGS__)
 #define SUSFS_LOGE(fmt, ...) if (susfs_is_log_enabled) pr_err("susfs:[%u][%d][%s]" fmt, current_uid().val, current->pid, __func__, ##__VA_ARGS__)
 #else
-#define SUSFS_LOGI(fmt, ...) 
-#define SUSFS_LOGE(fmt, ...) 
+#define SUSFS_LOGI(fmt, ...)
+#define SUSFS_LOGE(fmt, ...)
 #endif
 
 /* sus_path */
@@ -844,7 +844,7 @@ struct filename* susfs_get_redirected_path(unsigned long ino) {
 /* sus_su */
 #ifdef CONFIG_KSU_SUSFS_SUS_SU
 bool susfs_is_sus_su_hooks_enabled __read_mostly = false;
-static int susfs_sus_su_working_mode = 0;
+static int susfs_sus_su_working_mode = 2;
 extern void ksu_susfs_enable_sus_su(void);
 extern void ksu_susfs_disable_sus_su(void);
 
